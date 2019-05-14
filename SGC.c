b58c06d6ad97a9050f062de97672407e64afedb6
Simon Brown <s94brown@gmail.com>
2019-05-14 12:52:21 -0400

    SGC.c: added benchmarking

--- SGC.c
+++ SGC.c
@@ -5,26 +5,37 @@
 #include "data/idx_val.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
-int main() {
-    printf("in main\n");
-    float *new_features = malloc(2708*1433*sizeof(float));
+#define GRAPH_SIZE 2708
+#define FEATURE_DEPTH 1433
+#define DEGREE 2
+
+void full_matrix_mult () {
+    float *new_features = malloc(GRAPH_SIZE*FEATURE_DEPTH*sizeof(float));
     int d, i, j, k;
-    printf("var init\n");
-     for(d = 0; d < 2; d++){
-         printf("Degree: %d\n", d);
-         for(i = 0; i < 2708; i++) {
-             printf("Outer: %d\n", i);
-             for(j = 0; j < 1433; j++) {
-                 //printf("Inner: %d\n", j);
-                 new_features[i*1433 +j] = 0;
-                 for(k = 0; k < 2708; k++) {
-                     new_features[i*1433 + j] += ADJ[i][k] * FEAT[k][j];
+    for(d = 0; d < DEGREE; d++){
+        printf("Degree: %d\n", d);
+        for(i = 0; i < GRAPH_SIZE; i++) {
+            //printf("Outer: %d\n", i);
+            for(j = 0; j < FEATURE_DEPTH; j++) {
+                new_features[i*FEATURE_DEPTH +j] = 0;
+                for(k = 0; k < GRAPH_SIZE; k++) {
+                    new_features[i*FEATURE_DEPTH + j] += ADJ[i][k] * FEAT[k][j];
                 }
             }
         }
     }
     free(new_features);
-    return 0;
 }
 
+int main() {
+    clock_t begin = clock();
+    
+    full_matrix_mult();
+
+    clock_t end = clock();
+    double time_spent = (double)(end - begin) / CLOCKS_PER_SEC;
+    printf("Precomputation time: %lf\n", time_spent);
+    return 0;
+}
